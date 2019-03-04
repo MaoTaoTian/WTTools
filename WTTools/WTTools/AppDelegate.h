a0a86deb1e89e9c34c81579e1d376befba867a0d
tianmaotao <maotaotian@yeah.net>
2019-03-04 16:49:30 +0800

    init

---
+++ WTTools/WTTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTTools
+//
+//  Created by tianmaotao on 2019/3/4.
+//  Copyright © 2019年 tianmaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTTools/WTTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTTools
+//
+//  Created by tianmaotao on 2019/3/4.
+//  Copyright © 2019年 tianmaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
